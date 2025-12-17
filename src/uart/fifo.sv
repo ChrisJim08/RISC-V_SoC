60ac292cb036d419233467d4362669a8c2a7a3c7
Chris Jimenez <ChrisJimenez0818@gmail.com>
2025-12-17 01:57:36 -0600

    Added assertion to limit fifo depth to a power of 2 as required by wrap-bit validation

--- src/uart/fifo.sv
+++ src/uart/fifo.sv
@@ -1,6 +1,6 @@
 module fifo #(
   parameter  int unsigned DataWidth    = 32,
-  parameter  int unsigned Depth        = 16,
+  parameter  int unsigned Depth        = 16, // Depth must be a power of 2 due to wrap-bit validation method 
   localparam int unsigned PointerWidth = $clog2(Depth)
 ) (
   input logic                  clk_i,
@@ -17,10 +17,10 @@ module fifo #(
   logic [PointerWidth:0] rd_ptr, wr_ptr;
 
   // FIFO memory block
-  logic [DataWidth-1:0] mem [Depth-1:0];
+  logic [DataWidth-1:0]  mem [Depth-1:0];
   
   // Read/Write validation
-  logic  rd_valid, wr_valid;
+  logic  rd_valid , wr_valid;
   assign rd_valid = (rd_en_i && !empty_o);
   assign wr_valid = (wr_en_i && !full_o);
 
@@ -30,19 +30,23 @@ module fifo #(
 
   always_ff @(posedge clk_i) begin
     if(rst_i) begin
-      rd_ptr <= '0;
-      wr_ptr <= '0;
+      rd_ptr    <= '0;
+      wr_ptr    <= '0;
       rd_data_o <= '0;
     end 
     else begin
       if (rd_valid) begin
         rd_data_o <= mem[rd_ptr[PointerWidth-1:0]];
-        rd_ptr <= rd_ptr + 1;
+        rd_ptr    <= rd_ptr + 1;
       end
       if (wr_valid) begin
         mem[wr_ptr[PointerWidth-1:0]] <= wr_data_i;
-        wr_ptr <= wr_ptr + 1;
+        wr_ptr                        <= wr_ptr + 1;
       end 
     end
+  end
+
+  initial begin
+    if ((Depth & (Depth-1)) != 0) $error("FIFO Depth must be power of two");
   end
 endmodule
