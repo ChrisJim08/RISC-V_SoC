3a7fcc50635c9d357aa068f15918c6bb233865a6
Chris Jimenez <ChrisJimenez0818@gmail.com>
2025-12-17 15:12:06 -0600

    worked on uart rx ctrl (90% done)

--- src/uart/uart_rx.sv
+++ src/uart/uart_rx.sv
@@ -1,69 +1,93 @@
 module uart_rx #(
-  parameter int CLKS_PER_BIT = 87 //CLKS_PER_BIT = (Frequency of i_Clock)/(Frequency of UART (baud rate))
-) (
-  input  logic       clk_i,7
-  input  logic       rst_ni,
-  input  logic       rx_i,
-  output logic       rx_dv_o,
-  output logic [7:0] data_o
+  parameter   DataWidth  = 8,
+  localparam int unsigned CountWidth = $clog2(DataWidth)
+  ) (
+  input  logic                 clk_i,
+  input  logic                 rst_i,
+  input  logic                 tick_i,
+  input  logic                 rx_data_i,
+  output logic                 rx_dv_o,
+  output logic [DataWidth-1:0] data_o
 );
-
-  localparam integer CLOCK_COUNT_WIDTH = $clog2(CLKS_PER_BIT); //int or integer??????????????????????//
-  
-  typedef enum { 
-    Idle, StartBit, DataBits, StopBit //Reset/Cleanup state???????????????????
-  } uart_state_e;
-
-  // State Registers
-  uart_state_e cur_state_d;
-  uart_state_e nxt_state_q;
-
-  // FSM logic 
-  logic [2:0]                   bit_index;
-  logic [CLOCK_COUNT_WIDTH-1:0] clock_count;
 
   // rx data lines
   logic rx_data_r;
-  logic rx_data;
+  logic rxd;
 
   // Double register to reduce metastability
-  always_ff @(posedge clk_i) begin
-    rx_data_r <= rx_i;
-    rx_data   <= rx_data_r;
-  end
-
-  // Register State
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      cur_state_d <= Idle;
+  always_ff @(posedge clk_i or posedge rst_i) begin
+    if (rst_i) begin
+      rx_data_r <= 1;
+      rxd       <= 1;
     end else begin
-      cur_state_d <= nxt_state_q;
+      rx_data_r <= rx_data_i;
+      rxd       <= rx_data_r;
     end
-    
   end
 
-  // Next State Logic
-  always_comb begin
-    nxt_state_q = cur_state_d;
-    unique case (cur_state_d)
+  // States
+  typedef enum { 
+    Idle, StartBit, DataBits, StopBit 
+  } uart_state_e;
+
+  // State Registers
+  uart_state_e state_q;
+  uart_state_e state_d;
+
+  // Count register
+  logic [CountWidth:0] count;
+
+  // Shift buffer regisers
+  logic [DataWidth-1:0] sbuff_reg;
+
+  always_ff @(posedge clk_i or posedge rst_i) begin : memory_block
+    if (rst_i) begin
+      state_q   <= Idle;
+      sbuff_reg <= '0;
+      count     <=  0;
+    end else begin
+      state_q    <= state_d;
+      if (state_q == StartBit) begin
+        count <= 0;
+      end
+
+      if (tick_i && (state_q == DataBits)) begin
+        sbuff_reg <= {rxd, sbuff_reg[DataWidth-1:1]};
+        count <= count + 1;
+      end
+      if (state_q == StopBit) begin
+        data_o <= sbuff_reg;
+      end
+
+    end
+  end
+  
+  assign rx_dv_o = (state_q == StopBit);
+
+  always_comb begin 
+    state_d = state_q;
+    unique case (state_q)    
       Idle: begin
-        nxt_state_q = StartBit;
+        if (!rxd) begin
+          state_d = StartBit;
+        end 
       end
       StartBit: begin
-        nxt_state_q = DataBits;
+        if (tick_i) begin
+          state_d = DataBits;
+        end
       end
       DataBits: begin
-        nxt_state_q = StopBit;
+        if (count == DataWidth) begin
+          state_d = StopBit;
+        end
       end
       StopBit: begin
-        nxt_state_q = Idle;
+        state_d = Idle;
       end
       default: begin
-
       end
     endcase
   end
-
-  assign data_o = ;
 
 endmodule
