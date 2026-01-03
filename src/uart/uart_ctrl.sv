25b4a2af1e12f0d6c08f395daa3b50e012762ee2
Chris Jimenez <ChrisJimenez0818@gmail.com>
2026-01-02 19:46:55 -0600

    more toplevel work, got rid of _s for internal signals too

--- src/uart/uart_ctrl.sv
+++ src/uart/uart_ctrl.sv
@@ -1,6 +1,6 @@
 module uart_ctrl #(
   parameter int unsigned UartDataWidth  = 8,
-  parameter int unsigned BusDataWidth  = 32,
+  parameter int unsigned BusDataWidth   = 32,
   parameter int unsigned FifoDepth      = 16,
   parameter int unsigned ClockFrequency = 50_000_000,
   parameter int unsigned OverSampleRate = 16
@@ -21,45 +21,43 @@ module uart_ctrl #(
   // Should bus inputs be UartDataWidth?
   // Fix fifo rd and wr pointers to have d /q?
   // Internal signals
-  logic baud_clk_s;
-  logic rx_valid_s;
-  logic [UartDataWidth-1:0] rx_data_s;
-  logic rx_fifo_rd_en_s;
-  logic rx_fifo_full_s;
-  logic rx_fifo_empty_s;
-  logic [UartDataWidth-1:0] rx_fifo_rd_data_s; // Better name?
-  logic tx_valid_s;
-  assign tx_valid_s = ~tx_fifo_empty_s;
-  logic tx_fifo_wr_en_s;
-  logic tx_fifo_full_s;
-  logic tx_fifo_empty_s;
-  logic [UartDataWidth-1:0] tx_fifo_rd_data_s;
+  logic baud_clk;
+  logic rx_valid;
+  logic [UartDataWidth-1:0] rx_data;
+  logic rx_fifo_rd_en;
+  logic rx_fifo_full;
+  logic rx_fifo_empty;
+  logic tx_valid;
+  assign tx_valid = ~tx_fifo_empty;
+  logic tx_fifo_wr_en;
+  logic tx_fifo_rd_en;
+  assign tx_fifo_rd_en = ~tx_busy;
+  logic tx_fifo_full;
+  logic tx_fifo_empty;
+  logic [UartDataWidth-1:0] tx_fifo_rd_data;
 
   // Signal part-selecting
-  logic [1:0] baud_sel_s;
-  assign baud_sel_s = baud_rate_q[1:0];
   
   // TODO EXPLAIN THIS
-  logic rx_busy_s, rx_busy_r, tx_busy_s;
+  logic rx_busy, rx_busy_r, tx_busy;
   always_ff @(posedge clk_i or posedge rst_i) begin
     if (rst_i) begin
-      rx_busy_s <= 1'b0;
+      rx_busy <= 1'b0;
     end else begin
-      rx_busy_s <= rx_busy_r;
+      rx_busy <= rx_busy_r;
     end
   end
 
-  // Configurable (R/W) Baud Rate Register
-  logic [BusDataWidth-1:0] baud_rate_d;
-  logic [BusDataWidth-1:0] baud_rate_q;
+  // Configurable (R/W) Baud Rate Register                  // Change to uart config register
+  logic [1:0] baud_rate_d;
+  logic [1:0] baud_rate_q;
 
-  // Read-only Line Status Register
-  logic [BusDataWidth-1:0] status_reg;
-  assign status_reg = BusDataWidth'({tx_busy_s,rx_busy_s,rx_valid_s});
+  // Read-only Line Status Register                         // Add more flags
+  logic [2:0] status_reg;
+  assign status_reg = {tx_busy,rx_busy,rx_valid};
 
   // Read-only Reciever data Register (fifo rd_o)
-  //logic [BusDataWidth-1:0] rx_data_reg;             // Needed?
-  //assign rx_data_reg[7:0] = rx_fifo_rd_data_s;
+  logic [UartDataWidth-1:0] rx_fifo_rd_data;              // Better name?
 
   // Write-only Transmitter data Register
   logic [UartDataWidth-1:0] tx_data_d;
@@ -68,7 +66,7 @@ module uart_ctrl #(
   // Register block
   always_ff @(posedge clk_i or posedge rst_i) begin
     if (rst_i) begin
-      baud_rate_q <= '0;
+      baud_rate_q <= 2'b00;
       tx_data_q   <= '0;
     end else begin
       baud_rate_q <= baud_rate_d;
@@ -77,8 +75,8 @@ module uart_ctrl #(
   end
 
   always_comb begin 
-    rx_fifo_rd_en_s = 1'b0;
-    tx_fifo_wr_en_s = 1'b0;
+    rx_fifo_rd_en = 1'b0;
+    tx_fifo_wr_en = 1'b0;
     baud_rate_d = baud_rate_q;
     tx_data_d   = tx_data_q;
     bus_rdata_o = '0;
@@ -86,19 +84,19 @@ module uart_ctrl #(
     unique case (bus_addr_i[1:0])
       2'b00: begin 
         if (bus_wr_en_i) begin
-          baud_rate_d = bus_wdata_i; 
+          baud_rate_d      = bus_wdata_i[1:0]; 
         end else begin
-          bus_rdata_o = baud_rate_q;
+          bus_rdata_o[1:0] = baud_rate_q;
         end
       end
-      2'b01: bus_rdata_o = status_reg;
+      2'b01: bus_rdata_o = BusDataWidth'(status_reg);
       2'b10: begin
-        rx_fifo_rd_en_s = 1'b1;
-        bus_rdata_o = BusDataWidth'(rx_fifo_rd_data_s);           
+        rx_fifo_rd_en  = 1'b1;
+        bus_rdata_o      = BusDataWidth'(rx_fifo_rd_data);           
       end
       2'b11: begin 
         if (bus_wr_en_i) begin
-          tx_fifo_wr_en_s = 1'b1; 
+          tx_fifo_wr_en = 1'b1; 
           tx_data_d = bus_wdata_i[7:0]; 
         end
       end
@@ -112,10 +110,10 @@ module uart_ctrl #(
   ) baud_generator (
     .clk_i(clk_i),
     .rst_i(rst_i),
-    .rx_busy_i(rx_busy_s),
-    .tx_busy_i(tx_busy_s),
-    .baud_sel_i(baud_sel_s),
-    .baud_clk_o(baud_clk_s)
+    .rx_busy_i(rx_busy),
+    .tx_busy_i(tx_busy),
+    .baud_sel_i(baud_rate_q),
+    .baud_clk_o(baud_clk)
   );
 
   uart_rx #(
@@ -123,11 +121,11 @@ module uart_ctrl #(
   ) uart_reciever (
     .clk_i(clk_i),
     .rst_i(rst_i),
-    .baud_clk_i(baud_clk_s),
+    .baud_clk_i(baud_clk),
     .rxd_i(rxd_i),
-    .valid_o(rx_valid_s),
+    .valid_o(rx_valid),
     .busy_o(rx_busy_r),
-    .data_o(rx_data_s)
+    .data_o(rx_data)
   );
 
   fifo #(
@@ -136,12 +134,12 @@ module uart_ctrl #(
   ) recieve_fifo (
     .clk_i(clk_i),
     .rst_i(rst_i),
-    .rd_en_i(rx_fifo_rd_en_s),
-    .wr_en_i(rx_valid_s),
-    .wr_data_i(rx_data_s),
-    .full_o(rx_fifo_full_s),
-    .empty_o(rx_fifo_empty_s),
-    .rd_data_o(rx_fifo_rd_data_s) 
+    .rd_en_i(rx_fifo_rd_en),
+    .wr_en_i(rx_valid),
+    .wr_data_i(rx_data),
+    .full_o(rx_fifo_full),
+    .empty_o(rx_fifo_empty),
+    .rd_data_o(rx_fifo_rd_data) 
   );
 
   fifo #(
@@ -150,12 +148,12 @@ module uart_ctrl #(
   ) transmit_fifo (
     .clk_i(clk_i),
     .rst_i(rst_i),
-    .wr_en_i(tx_fifo_wr_en_s),
-    .rd_en_i(1'b0),                     // Tie to ground?
+    .wr_en_i(tx_fifo_wr_en),
+    .rd_en_i(tx_fifo_rd_en),                     
     .wr_data_i(tx_data_q),
-    .full_o(tx_fifo_full_s),
-    .empty_o(tx_fifo_empty_s),
-    .rd_data_o(tx_fifo_rd_data_s)
+    .full_o(tx_fifo_full),
+    .empty_o(tx_fifo_empty),
+    .rd_data_o(tx_fifo_rd_data)
   );
 
   uart_tx #(
@@ -163,11 +161,11 @@ module uart_ctrl #(
   ) uart_transmitter (
     .clk_i(clk_i),
     .rst_i(rst_i),
-    .baud_clk_i(baud_clk_s),
-    .dv_i(tx_valid_s),
-    .data_i(tx_fifo_rd_data_s),
+    .baud_clk_i(baud_clk),
+    .dv_i(tx_valid),
+    .data_i(tx_fifo_rd_data),
     .txd_o(txd_o), 
-    .busy_o(tx_busy_s)
+    .busy_o(tx_busy)
   );
 
 endmodule
