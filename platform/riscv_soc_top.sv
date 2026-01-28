b145c9eb4f99da5acebc3257460ab5a2fc089a22
Chris Jimenez <ChrisJimenez0818@gmail.com>
2026-01-28 01:54:46 -0600

    created toplevel module, for SoC simulation, to help me picture the bus logic implementation .

---
+++ platform/riscv_soc_top.sv
@@ -0,0 +1,65 @@
+module riscv_soc_top #(
+  localparam int unsigned AddressWidth = 10,
+  localparam int unsigned DataWidth    = 32,
+  localparam int unsigned NumTargets   = 2
+)(
+  input logic                     clk_i,
+  input logic                     rst_i, 
+  input logic                     imem_load_i, 
+  input logic  [AddressWidth-1:0] imem_load_addr_i, 
+  input logic  [DataWidth-1:0]    imem_load_data_i,
+  output logic                    halt_o  
+);
+
+// Instruction memory signals
+  logic [AddressWidth-1:0] imem_addr;
+  logic [AddressWidth-1:0] core_imem_addr;
+  logic [DataWidth-1:0]    instr;
+
+  assign imem_addr = imem_load_i ? imem_load_addr_i : core_imem_addr;
+
+// Data memory signals
+  logic [DataWidth-1:0]    dmem_r_data;
+  logic                    dmem_wr_en;
+  logic [AddressWidth-1:0] dmem_addr;
+  logic [DataWidth-1:0]    dmem_wr_data;
+
+// SoC
+  riscv_soc #(
+    .AddressWidth(AddressWidth),
+    .DataWidth(DataWidth)
+  ) riscv_core (
+    .clk_i(clk_i),
+    .rst_i(rst_i),
+    .instr_i(instr),
+    .imem_addr_o(core_imem_addr),
+    .dmem_r_data_i(dmem_r_data),
+    .dmem_wr_en_o(dmem_wr_en),
+    .dmem_addr_o(dmem_addr),
+    .dmem_wr_data_o(dmem_wr_data),
+    .halt_o(halt_o)
+  );
+
+    mem #(
+    .AddressWidth(AddressWidth),
+    .DataWidth(DataWidth)
+  ) imem (
+    .clk_i(clk_i),
+    .wr_en_i(imem_load_i),
+    .addr_i(imem_addr),
+    .wr_data_i(imem_load_data_i),
+    .r_data_o(instr)
+  );
+
+  mem #(
+    .AddressWidth(AddressWidth),
+    .DataWidth(DataWidth)
+  ) dmem (
+    .clk_i(clk_i),
+    .wr_en_i(dmem_wr_en),
+    .addr_i(dmem_addr),
+    .wr_data_i(dmem_wr_data),
+    .r_data_o(dmem_r_data)
+  );
+
+endmodule
--- src/riscv_soc.sv
+++ src/riscv_soc.sv
@@ -7,9 +7,18 @@ module riscv_soc #(
 )(
   input  logic                    clk_i,
   input  logic                    rst_i, 
-  input  logic                    imem_load_i, 
-  input  logic [AddressWidth-1:0] imem_load_addr_i, 
-  input  logic [DataWidth-1:0]    imem_load_data_i,
+
+// Instruction memory ports
+  input  logic [DataWidth-1:0]    instr_i,
+  output logic [AddressWidth-1:0] core_imem_addr_o, 
+
+// dmem (RAM) ports
+  input  logic [DataWidth-1:0]    dmem_r_data_i,
+  output logic                    dmem_wr_en_o,
+  output logic [AddressWidth-1:0] dmem_addr_o,
+  output logic [DataWidth-1:0]    dmem_wr_data_o,
+
+// Simulation signal
   output logic                    halt_o  
 );
 
@@ -26,22 +35,25 @@ module riscv_soc #(
     .NumTargets(NumTargets)
   ) bus_fabric ( 
     .core_if(core_if),
-    .s_if(targets)
+    .s_if(target_ifs)
   );
 
-  
+// Core's bus adapter
+
+// SoC core
   riscv_core #(
     .AddressWidth(AddressWidth),
     .DataWidth(DataWidth)
   ) riscv_core (
     .clk_i(clk_i),
     .rst_i(rst_i),
-    .instr_i(instr),
-    .imem_addr_o(core_imem_addr),
-    .dmem_r_data_i(dmem_r_data),
-    .dmem_wr_en_o(dmem_wr_en),
-    .dmem_addr_o(dmem_addr),
-    .dmem_wr_data_o(dmem_wr_data),
+    .instr_i(instr_i),
+    .imem_addr_o(core_imem_addr_o),
+    .dmem_r_data_i(dmem_r_data_i),
+    .dmem_wr_en_o(dmem_wr_en_o),
+    .dmem_addr_o(dmem_addr_o),
+    .dmem_wr_data_o(dmem_wr_data_o),
     .halt_o(halt_o)
   );
+
 endmodule
