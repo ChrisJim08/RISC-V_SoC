6aaea9cc4eeed7875a474418b7e992b4448f0e8d
Chris Jimenez <ChrisJimenez0818@gmail.com>
2026-02-04 04:28:53 -0600

    added logic to allow independent w and aw channels. (Allowing a write onto W channel before the address write onto AW channel

--- src/system_bus/bus_fabric.sv
+++ src/system_bus/bus_fabric.sv
@@ -14,10 +14,12 @@ module bus_fabric #(
 // Internal Signals
 
   // Status flags
+  logic aw_seen, w_seen;
+  logic write_busy;
   logic wr_inflight, rd_inflight; // TODO add/remove any (!)inflight validation?
 
   // Handshake flags
-  logic aw_handshake, b_handshake, ar_handshake, r_handshake;
+  logic aw_handshake, w_handshake, b_handshake, ar_handshake, r_handshake;
 
   // One-hot select lines
   logic [NumTargets-1:0] wr_sel_onehot_d;
@@ -30,6 +32,10 @@ module bus_fabric #(
 // Handshake flag logic
   // Write address (AW)
   assign aw_handshake = core_if.aw_valid && core_if.aw_ready;
+
+  // Write data (W)
+
+  assign w_handshake = core_if.w_ready && core_if.w_valid;
   
   // Write response (B)
   assign b_handshake = core_if.b_ready && core_if.b_valid;
@@ -40,29 +46,39 @@ module bus_fabric #(
   // Read data (R)
   assign r_handshake = core_if.r_ready && core_if.r_valid;
   
-// One-hot sel / inflight logic
+// One-hot sel / Seen flag logic
   always_ff @(posedge clk_i or posedge rst_i) begin
     if (rst_i) begin
-      wr_inflight <= 1'b0;
+      aw_seen <= 1'b0;
+      w_seen  <= 1'b0;
       rd_inflight <= 1'b0;
       wr_sel_onehot_q <= '0;
       rd_sel_onehot_q <= '0;
 
     end else begin
       if (aw_handshake) begin
-        wr_inflight  <= 1'b1;
+        aw_seen <= 1'b1;
       end
       
-      if (b_handshake) begin
-        wr_inflight <= 1'b0;
+      if (w_handshake) begin
+        w_seen <= 1'b1;
+        // if (!aw_seen) latch data and strobe
+        // w_data_d <= core_if.w_data; 
+      end
+      
+      if (b_handshake && wr_busy) begin
+        aw_seen <= 1'b0;
+        w_seen  <= 1'b0;
+        wr_sel_onehot_q <= '0;
       end
 
-      if (ar_handshake) begin
+      if (ar_handshake && !rd_inflight) begin
         rd_inflight  <= 1'b1;
       end
       
       if (r_handshake) begin
         rd_inflight <= 1'b0;
+        rd_sel_onehot_q <= '0;
       end
 
     // Latch target during AW / AR handshake
@@ -76,8 +92,12 @@ module bus_fabric #(
   end
   
   // Use latched selection while transaction is in-flight
-  assign wr_sel_onehot = wr_inflight ? wr_sel_onehot_q : wr_sel_onehot_d;
+  assign wr_sel_onehot = aw_seen ? wr_sel_onehot_q : wr_sel_onehot_d;
   assign rd_sel_onehot = rd_inflight ? rd_sel_onehot_q : rd_sel_onehot_d;
+
+// In-flight / Busy flag logic
+  assign wr_busy     = aw_seen && w_seen;
+  assign wr_inflight = aw_seen || w_seen;
 
 // Forward routing
   always_comb begin : target_demux
@@ -107,18 +127,21 @@ module bus_fabric #(
     // Write channels
       if (wr_sel_onehot[i]) begin
       // AW
-        if (!wr_inflight) begin
+        if (!aw_seen) begin
           target_ifs[i].aw_valid = core_if.aw_valid;
           target_ifs[i].aw_addr  = core_if.aw_addr;
         end
 
       // W
-        target_ifs[i].w_valid = core_if.w_valid;
-        target_ifs[i].w_data  = core_if.w_data;
-        target_ifs[i].w_strb  = core_if.w_strb;
-
+        if (!w_seen) begin
+          target_ifs[i].w_valid = core_if.w_valid;
+          target_ifs[i].w_data  = core_if.w_data;
+          target_ifs[i].w_strb  = core_if.w_strb;
+        end 
       // B
-        target_ifs[i].b_ready = core_if.b_ready;
+        if (wr_busy) begin
+          target_ifs[i].b_ready = core_if.b_ready;
+        end
       end
        
     // Read channels
@@ -159,9 +182,9 @@ module bus_fabric #(
     // Write channels
       if (wr_sel_onehot[j]) begin
       // AW
-        core_if.aw_ready = wr_inflight ? 1'b0 : target_ifs[j].aw_ready;
+        core_if.aw_ready = aw_seen ? 1'b0 : target_ifs[j].aw_ready;
       // W
-        core_if.w_ready  = target_ifs[j].w_ready;
+        core_if.w_ready  = w_seen ? 1'b0 : target_ifs[j].w_ready;
       // B
         core_if.b_valid  = target_ifs[j].b_valid;
         core_if.b_resp   = target_ifs[j].b_resp;
