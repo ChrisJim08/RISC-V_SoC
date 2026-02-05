d84d0338324bcc6a3d1ec8eb2737af5d752adb21
Chris Jimenez <ChrisJimenez0818@gmail.com>
2026-02-05 16:35:01 -0600

    added more logic to differentiate different (W) handshakes. (buffered/unbuffered)

--- src/system_bus/bus_fabric.sv
+++ src/system_bus/bus_fabric.sv
@@ -1,9 +1,10 @@
 //  Module: bus_fabric
 //
 module bus_fabric #(
-  parameter int unsigned AddressWidth = 32,
-  parameter int unsigned DataWidth    = 32,
-  parameter int unsigned NumTargets   = 2
+  parameter int unsigned  AddressWidth = 32,
+  parameter int unsigned  DataWidth    = 32,
+  localparam int unsigned StrobeWidth  = DataWidth / 8,
+  parameter int unsigned  NumTargets   = 2
 )( 
   input logic clk_i,
   input logic rst_i,
@@ -11,14 +12,20 @@ module bus_fabric #(
   bus_if.target    target_ifs [NumTargets]
 );
   // Write buffer
-  logic w_buffered, w_buffer_full;
-
   logic w_before_aw;
+
+  logic                   use_w_buf;
+  logic                   drain_w_buf;
+  logic                   buff_full;
+  logic [DataWidth-1:0]   buff_w_data;
+  logic [StrobeWidth-1:0] buff_w_strb;
+
+  logic buff_w_valid, buff_w_ready;
 
 // Internal Signals
 
   // Status flags
-  logic aw_seen, w_seen;
+  logic aw_seen, w_seen; // w_seen = target has accepted buffered write data (Write Data Target Handshake)
   logic write_busy;
   logic wr_inflight, rd_inflight; // TODO add/remove any (!)inflight validation?
 
@@ -48,6 +55,11 @@ module bus_fabric #(
   
   // Read data (R)
   assign r_handshake = core_if.r_ready && core_if.r_valid;
+
+  // Write Data before Address flag
+  assign w_before_aw = !aw_seen && !w_seen && core_if.w_valid;
+
+  assign buff_w_ready = (w_before_aw && !buff_full); // ready signal to mimic a target recieving data to connect to core_if.w_ready's if before AW
   
 // One-hot sel / Seen flag logic
   always_ff @(posedge clk_i or posedge rst_i) begin
@@ -58,28 +70,33 @@ module bus_fabric #(
       wr_sel_onehot_q <= '0;
       rd_sel_onehot_q <= '0;
 
-      w_data_buffer <= '0;
-      w_strb_buffer <= '0;
-      w_buffer_full <= 1'b0;
+      buff_w_data  <= '0;
+      buff_w_strb  <= '0;
+      buff_full  <= 1'b0;
 
-    end else begin 
-      // Handshake and latch w_data and w_strb 
-      // When address has not been given
-      if (!aw_seen && !w_seen && core_if.w_valid && !w_buffer_full) begin
-        core_if.w_ready <= 1'b1;
-        w_data_buffer <= core_if.w_data;
-        w_strb_buffer <= core_if.w_strb;
-        w_buffer_full   <= 1'b1;
-      end
-
-      if (aw_handshake && !aw_seen) begin // Redundant?
+    end else begin
+      if (aw_handshake && !aw_seen) begin // Redundant !aw_seen?
         aw_seen <= 1'b1; 
                 
         wr_sel_onehot_q <= wr_sel_onehot_d;
       end
       
-      if (w_handshake && !w_seen) begin
-        w_seen <= 1'b1;
+      if (w_handshake && !w_seen) begin // Redundant !w_seen?
+        // Handshake and latch w_data and w_strb when address has not been given
+        if (buff_w_ready) begin
+          buff_w_valid <= 1'b1;
+          buff_w_data  <= core_if.w_data;
+          buff_w_strb  <= core_if.w_strb;
+          buff_full  <= 1'b1;
+        end else begin
+          w_seen <= 1'b1;
+          if (buff_w_valid) begin
+            buff_w_valid <= 1'b1;
+            buff_w_data  <= '0;
+            buff_w_strb  <= '0;
+            buff_full  <= 1'b0;
+          end
+        end
       end
       
       if (b_handshake && wr_inflight) begin
@@ -87,11 +104,6 @@ module bus_fabric #(
         w_seen  <= 1'b0;
         wr_sel_onehot_q <= '0;
 
-        if (w_buffer_full) begin
-          w_data_buffer <= '0;
-          w_strb_buffer <= '0;
-          w_buffer_full <= 1'b0;
-        end
       end
 
       if (ar_handshake && !rd_inflight) begin
@@ -148,20 +160,24 @@ module bus_fabric #(
         end
 
       // W
-        if (!w_seen) begin //*
-          target_ifs[i].w_valid = core_if.w_valid; // buffered ?
-          target_ifs[i].w_data  = core_if.w_data;  // buffered ?
-          target_ifs[i].w_strb  = core_if.w_strb;  // buffered ?
-        end else if (wr_inflight && buffered) begin
-          target_ifs[i].w_valid = buff_w_valid; 
-          target_ifs[i].w_data  = buff_w_data;  
-          target_ifs[i].w_strb  = buff_w_strb;  
+        if (!w_seen) begin
+          if (buff_full) begin 
+            target_ifs[i].w_valid = buff_w_valid; 
+            target_ifs[i].w_data  = buff_w_data;  
+            target_ifs[i].w_strb  = buff_w_strb;  
+          end else begin
+            target_ifs[i].w_valid = core_if.w_valid;
+            target_ifs[i].w_data  = core_if.w_data; 
+            target_ifs[i].w_strb  = core_if.w_strb; 
+          end
         end
+
       // B
-        if (wr_inflight) begin //*
+        if (wr_inflight) begin
           target_ifs[i].b_ready = core_if.b_ready;
         end
       end
+
        
     // Read channels
       if (rd_sel_onehot[i]) begin
@@ -203,8 +219,9 @@ module bus_fabric #(
       // AW
         core_if.aw_ready = aw_seen ? 1'b0 : target_ifs[j].aw_ready;
       // W
-        core_if.w_ready  = w_seen ? 1'b0 : target_ifs[j].w_ready;     // Add logic to handshake for W before AW
-        // (w_before_aw ? w_ready_buffer : target_ifs[j].w_ready);
+        if (!w_seen) begin 
+          core_if.w_ready = (w_before_aw && !buff_full) ? buff_w_ready : target_ifs[j].w_ready; // Redunancy as the expression is buff_w_ready's defenition
+        end
       // B
         core_if.b_valid  = target_ifs[j].b_valid;
         core_if.b_resp   = target_ifs[j].b_resp;
