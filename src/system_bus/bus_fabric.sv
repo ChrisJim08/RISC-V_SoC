dd274e1074dd9608644f31427e24cacb846b5477
Chris Jimenez <ChrisJimenez0818@gmail.com>
2026-02-06 02:59:06 -0600

    redid my logic for W before AW buffer filling and draining by adding flags and handshake signals for each

--- src/system_bus/bus_fabric.sv
+++ src/system_bus/bus_fabric.sv
@@ -12,20 +12,20 @@ module bus_fabric #(
   bus_if.target    target_ifs [NumTargets]
 );
   // Write buffer
-  logic w_before_aw;
 
   logic                   use_w_buf;
   logic                   drain_w_buf;
+  logic                   w_direct_hs;
+  logic                   w_buf_fill_hs;
+  logic                   w_buf_drain_hs;
   logic                   buff_full;
   logic [DataWidth-1:0]   buff_w_data;
   logic [StrobeWidth-1:0] buff_w_strb;
 
-  logic buff_w_valid, buff_w_ready;
-
 // Internal Signals
 
   // Status flags
-  logic aw_seen, w_seen; // w_seen = target has accepted buffered write data (Write Data Target Handshake)
+  logic aw_hs_seen, w_hs_seen; // w_hs_seen = target has accepted buffered write data (Write Data Target Handshake)
   logic write_busy;
   logic wr_inflight, rd_inflight; // TODO add/remove any (!)inflight validation?
 
@@ -46,7 +46,13 @@ module bus_fabric #(
 
   // Write data (W)
   assign w_handshake = core_if.w_ready && core_if.w_valid;
+
+  // Write data (W) directly
+  assign w_direct_hs = w_handshake && !use_w_buf;
   
+  // Write data (W) fill buffer
+  assign w_buf_fill_hs = w_handshake && use_w_buf;
+
   // Write response (B)
   assign b_handshake = core_if.b_ready && core_if.b_valid;
   
@@ -56,16 +62,17 @@ module bus_fabric #(
   // Read data (R)
   assign r_handshake = core_if.r_ready && core_if.r_valid;
 
-  // Write Data before Address flag
-  assign w_before_aw = !aw_seen && !w_seen && core_if.w_valid;
+// Write Data (W) before Address (AW) flags
+  assign use_w_buf = core_if.w_valid && !aw_hs_seen && !w_hs_seen && !buff_full;
 
-  assign buff_w_ready = (w_before_aw && !buff_full); // ready signal to mimic a target recieving data to connect to core_if.w_ready's if before AW
-  
+  assign drain_w_buf = buff_full && aw_hs_seen;
+
+
 // One-hot sel / Seen flag logic
   always_ff @(posedge clk_i or posedge rst_i) begin
     if (rst_i) begin
-      aw_seen <= 1'b0;
-      w_seen  <= 1'b0;
+      aw_hs_seen <= 1'b0;
+      w_hs_seen  <= 1'b0;
       rd_inflight <= 1'b0;
       wr_sel_onehot_q <= '0;
       rd_sel_onehot_q <= '0;
@@ -75,33 +82,35 @@ module bus_fabric #(
       buff_full  <= 1'b0;
 
     end else begin
-      if (aw_handshake && !aw_seen) begin // Redundant !aw_seen?
-        aw_seen <= 1'b1; 
+      if (aw_handshake && !aw_hs_seen) begin // Redundant !aw_hs_seen?
+        aw_hs_seen <= 1'b1; 
                 
         wr_sel_onehot_q <= wr_sel_onehot_d;
       end
-      
-      if (w_handshake && !w_seen) begin // Redundant !w_seen?
-        // Handshake and latch w_data and w_strb when address has not been given
-        if (buff_w_ready) begin
-          buff_w_valid <= 1'b1;
+
+      if (!w_hs_seen) begin 
+        if (w_buf_fill_hs) begin
           buff_w_data  <= core_if.w_data;
           buff_w_strb  <= core_if.w_strb;
           buff_full  <= 1'b1;
-        end else begin
-          w_seen <= 1'b1;
-          if (buff_w_valid) begin
-            buff_w_valid <= 1'b1;
-            buff_w_data  <= '0;
-            buff_w_strb  <= '0;
-            buff_full  <= 1'b0;
+        end
+
+        if (w_direct_hs || w_buf_drain_hs) begin
+          w_hs_seen <= 1'b1;
+
+          if (drain_w_buf) begin
+            buff_w_data <= '0;
+            buff_w_strb <= '0;
+            buff_full   <= 1'b0;
+            drain_w_buf <= 1'b0;
           end
         end
+
       end
       
       if (b_handshake && wr_inflight) begin
-        aw_seen <= 1'b0;
-        w_seen  <= 1'b0;
+        aw_hs_seen <= 1'b0;
+        w_hs_seen  <= 1'b0;
         wr_sel_onehot_q <= '0;
 
       end
@@ -120,15 +129,17 @@ module bus_fabric #(
   end
   
   // Use latched selection while transaction is in-flight                 // Redundant?
-  assign wr_sel_onehot = aw_seen ? wr_sel_onehot_q : wr_sel_onehot_d;
+  assign wr_sel_onehot = aw_hs_seen ? wr_sel_onehot_q : wr_sel_onehot_d;
   assign rd_sel_onehot = rd_inflight ? rd_sel_onehot_q : rd_sel_onehot_d;
 
 // In-flight / Busy flag logic
-  assign wr_inflight = aw_seen && w_seen;
+  assign wr_inflight = aw_hs_seen && w_hs_seen;
 
 // Forward routing
   always_comb begin : target_demux
   // Defaults
+    w_buf_drain_hs = 0'b0;
+
     for (int i = 0; i < NumTargets; i++) begin
     // Write channels
       // AW
@@ -153,16 +164,19 @@ module bus_fabric #(
     for (int i = 0; i < NumTargets; i++) begin 
     // Write channels
       if (wr_sel_onehot[i]) begin
+      // Write buffer handshake
+        w_buf_drain_hs = target_ifs[i].w_ready&& !w_hs_seen && drain_w_buf;
+
       // AW
-        if (!aw_seen) begin //*
+        if (!aw_hs_seen) begin //*
           target_ifs[i].aw_valid = core_if.aw_valid;
           target_ifs[i].aw_addr  = core_if.aw_addr;
         end
 
       // W
-        if (!w_seen) begin
-          if (buff_full) begin 
-            target_ifs[i].w_valid = buff_w_valid; 
+        if (!w_hs_seen) begin
+          if (buff_full && aw_seen) begin // redundant aw_seen? (since wr_sel_onehot isn't chosen until aw is seen) 
+            target_ifs[i].w_valid = 1'b1; 
             target_ifs[i].w_data  = buff_w_data;  
             target_ifs[i].w_strb  = buff_w_strb;  
           end else begin
@@ -213,21 +227,26 @@ module bus_fabric #(
     core_if.r_data = '0;
     core_if.r_resp = '0;
 
-    for (int j = 0; j < NumTargets; j++) begin
-    // Write channels
-      if (wr_sel_onehot[j]) begin
-      // AW
-        core_if.aw_ready = aw_seen ? 1'b0 : target_ifs[j].aw_ready;
-      // W
-        if (!w_seen) begin 
-          core_if.w_ready = (w_before_aw && !buff_full) ? buff_w_ready : target_ifs[j].w_ready; // Redunancy as the expression is buff_w_ready's defenition
-        end
-      // B
-        core_if.b_valid  = target_ifs[j].b_valid;
-        core_if.b_resp   = target_ifs[j].b_resp;
+    if (use_w_buf) begin  // When AW has not been seen and buffer is not full
+      core_if.w_ready = 1'b1;
+
+    end else begin
+      for (int j = 0; j < NumTargets; j++) begin
+      // Write channels
+        if (wr_sel_onehot[j]) begin
+        // AW
+          core_if.aw_ready = aw_hs_seen ? 1'b0 : target_ifs[j].aw_ready;
+        // W
+          core_if.w_ready = w_hs_seen ? 1'b0 : target_ifs[j].w_ready;
+        // B
+          core_if.b_valid  = target_ifs[j].b_valid;
+          core_if.b_resp   = target_ifs[j].b_resp;
+        end 
       end
-      
-    // Read channels
+    end
+
+  // Read channels
+    for (int j = 0; j < NumTargets; j++) begin
       if (rd_sel_onehot[j]) begin
       // AR
         core_if.ar_ready = rd_inflight ? 1'b0 : target_ifs[j].ar_ready;
