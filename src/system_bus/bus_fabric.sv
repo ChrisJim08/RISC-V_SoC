e151505586cac1a9bd819f87e1dc83f890043cfd
Chris Jimenez <ChrisJimenez0818@gmail.com>
2026-02-07 16:08:06 -0600

    added/cleaned comments, got rid of the old unused buffer signals, added skid_ready signal, cleaned latching block

--- src/system_bus/bus_fabric.sv
+++ src/system_bus/bus_fabric.sv
@@ -13,15 +13,14 @@ module bus_fabric #(
 );
 // Write buffer
   logic skid_full;
-  logic skid_ready;
+  logic skid_w_ready;
+  assign skid_w_ready = !skid_full;
   logic [DataWidth-1:0]   skid_data;
   logic [StrobeWidth-1:0] skid_strb;
-  //logic                   use_w_buf;  // TODO: FSM States?
-  //logic                   drain_w_buf;
-  //logic                   buff_full;
-  //logic [DataWidth-1:0]   buff_w_data;
-  //logic [StrobeWidth-1:0] buff_w_strb;
 
+  logic target_if_aw_ready;
+  logic target_if_w_ready;
+  logic target_if_w_valid;
 // Internal Signals
   // Status flags
   logic aw_hs_seen, w_hs_seen; 
@@ -40,14 +39,11 @@ module bus_fabric #(
   logic [NumTargets-1:0] rd_sel_onehot;
 
 // Handshake flags
-  // Write address (AW)
-  assign aw_handshake = core_if.aw_valid && core_if.aw_ready;
-  
-  // Write data (W) fill buffer
-  assign w_buf_fill_hs = core_if.w_ready && core_if.w_valid && use_w_buf;
-
+  // Write address (AW)                                               // TODO change all handshake signals to use
+  assign aw_handshake = core_if.aw_valid && core_if.aw_ready;          // core_if.* && target_if_*;
+                                                                      // one signal from each (target and core)?
   // Write response (B)
-  assign b_handshake = core_if.b_ready && core_if.b_valid;
+  assign b_handshake = core_if.b_ready && core_if.b_valid;           
   
   // Read address (AR)
   assign ar_handshake = core_if.ar_valid && core_if.ar_ready;
@@ -58,11 +54,7 @@ module bus_fabric #(
 // In-flight flag
   assign wr_inflight = aw_hs_seen && w_hs_seen;
 
-// Write Data (W) buffer flags
-  assign use_w_buf = core_if.w_valid && !aw_hs_seen && !w_hs_seen && !buff_full && !core_if.aw_valid; // Schronous AW/W support
-  assign drain_w_buf = buff_full && aw_hs_seen;
-
-// Latched selection while transaction is in-flight                     // Redundant?
+// Latched selection while transaction is in-flight                     // TODO Review Logic (skid buffer?)
   assign wr_sel_onehot = aw_hs_seen ? wr_sel_onehot_q : wr_sel_onehot_d;
   assign rd_sel_onehot = rd_inflight ? rd_sel_onehot_q : rd_sel_onehot_d;
 
@@ -79,40 +71,34 @@ module bus_fabric #(
       skid_strb <= '0;
       skid_full <= 1'b0;
 
-      skid_target <= 0;
     end else begin
-      if (aw_handshake && !aw_hs_seen) begin
+      // Lock target when 
+      if (aw_handshake && !aw_hs_seen) begin                        // TODO Should i still be using _seen flags
         aw_hs_seen <= 1'b1; 
                 
         wr_sel_onehot_q <= wr_sel_onehot_d;
       end
 
-
-      if (core_if.w_valid && !target_ifs_w_ready && !skid_full) begin
+    // Buffer logic, if space, when target is not ready
+      if (core_if.w_valid && !target_if_w_ready && skid_w_ready) begin
         skid_data  <= core_if.w_data;
         skid_strb  <= core_if.w_strb;
         skid_full  <= 1'b1;
       end
 
-    // Target accepted buffered write data 
-      if (w_buf_drain_hs) begin
+    // Target accepted write data 
+      if (target_if_w_valid && target_if_w_ready) begin         // TODO Review logic (w_handshake?)
         w_hs_seen   <= 1'b1;
         buff_w_data <= '0;
         buff_w_strb <= '0;
         buff_full   <= 1'b0;
       end
 
-    // Target accepted write data from Initator 
-      if (w_direct_hs) begin
-        w_hs_seen <= 1'b1;
-      end
-
-      
       if (b_handshake && wr_inflight) begin
         aw_hs_seen <= 1'b0;
         w_hs_seen  <= 1'b0;
-        wr_sel_onehot_q <= '0;
 
+        wr_sel_onehot_q <= '0;                          
       end
 
       if (ar_handshake && !rd_inflight) begin
@@ -136,10 +122,6 @@ module bus_fabric #(
   // Forward routing
       always_comb begin : target_demux
     // Defaults
-      //Write handshakes
-        w_direct_hs    = 0'b0;
-        w_buf_drain_hs = 0'b0;
-
       // Write channels
           // AW
         target_ifs[g].aw_valid = 1'b0;
@@ -164,9 +146,8 @@ module bus_fabric #(
         // AW
             target_ifs[g].aw_valid = core_if.aw_valid;
             target_ifs[g].aw_addr  = core_if.aw_addr;
-
         // W
-          if (skid_full) begin 
+          if (skid_full) begin                              // TODO bypass?
             target_ifs[g].w_valid = 1'b1; 
             target_ifs[g].w_data  = skid_data;  
             target_ifs[g].w_strb  = skid_strb;  
@@ -175,13 +156,11 @@ module bus_fabric #(
             target_ifs[g].w_data  = core_if.w_data; 
             target_ifs[g].w_strb  = core_if.w_strb; 
           end
-        
         // B
           if (wr_inflight) begin                              // inflight needed?
             target_ifs[g].b_ready = core_if.b_ready;
           end
         end
-        
 
       // Read channels
         if (rd_sel_onehot[g]) begin
@@ -201,9 +180,9 @@ module bus_fabric #(
     // Defaults
       // Write channels
         // AW
-        core_if.aw_ready = 1'b0;
+        core_if.aw_ready = 1'b0;          
         // W
-        core_if.w_ready = 1'b0;
+        core_if.w_ready = 1'b0;                 // TODO Defaults needed?
         // B
         core_if.b_valid = 1'b0;
         core_if.b_resp = '0;
@@ -217,13 +196,13 @@ module bus_fabric #(
         core_if.r_resp = '0;
 
       // Write channels
-
         // W      
-        core_if.w_ready = wr_sel_onehot[g] ? target_ifs[g].w_ready : !skid_full;   // && !w_hs_seen?
-        target_ifs_w_ready = wr_sel_onehot[g] ? target_ifs[g].w_ready : 1'b0;
+        target_if_aw_ready = wr_sel_onehot[g] ? target_ifs[g].aw_ready : 1'b0;
+        target_if_w_ready  = wr_sel_onehot[g] ? target_ifs[g].w_ready : 1'b0;
+        core_if.w_ready     = wr_sel_onehot[g] ? target_ifs[g].w_ready : skid_w_ready; 
         if (wr_sel_onehot[g]) begin
         // AW
-          core_if.aw_ready = aw_hs_seen ? 1'b0 : target_ifs[g].aw_ready; // Redundant aw_hs_seen?
+          core_if.aw_ready = target_ifs[g].aw_ready;
         // B
           core_if.b_valid  = target_ifs[g].b_valid;
           core_if.b_resp   = target_ifs[g].b_resp;
