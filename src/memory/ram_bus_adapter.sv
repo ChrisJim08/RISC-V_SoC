dac5e0926a2450d9467b490e43a7a81230d71de1
Chris Jimenez <ChrisJimenez0818@gmail.com>
2026-02-20 00:45:09 -0600

    finished ram's bus adapter

--- src/memory/ram_bus_adapter.sv
+++ src/memory/ram_bus_adapter.sv
@@ -2,8 +2,9 @@
 //  Bus adapter enabling my mem block to interface with the bus
 
 module ram_bus_adapter #(
-  parameter DataWidth    = 32,
-  parameter AddressWidth = 32
+  parameter  int unsigned DataWidth    = 32,
+  parameter  int unsigned AddressWidth = 32,
+  localparam int unsigned NumBytes     = DataWidth / 8
 ) (
 // System
   input logic clk_i,
@@ -11,6 +12,7 @@ module ram_bus_adapter #(
 
 // Memory block
   output logic                    mem_wr_en_o,
+  output logic [NumBytes-1:0]     mem_byte_en_o,
   output logic [AddressWidth-1:0] mem_addr_o,
   output logic [DataWidth-1:0]    mem_wr_data_o,
   input  logic [DataWidth-1:0]    mem_r_data_i,
@@ -23,12 +25,14 @@ module ram_bus_adapter #(
   logic w_hs;
   logic b_hs;
   logic ar_hs;
+  logic ar_hs_d1; // Delayed 1 cycle
   logic r_hs;
 
   logic aw_seen;
   logic w_seen;
 
   logic [AddressWidth-1:0] aw_addr_q;
+  logic [NumBytes-1:0]     w_strb_q;
   logic [DataWidth-1:0]    w_data_q;
   logic [AddressWidth-1:0] ar_addr_q;
 
@@ -41,14 +45,17 @@ module ram_bus_adapter #(
   assign ar_hs = bus_if.ar_valid && bus_if.ar_ready;
   assign r_hs  = bus_if.r_ready  && bus_if.r_valid;
 
-  assign bus_if.aw_ready = !aw_seen     && !rd_inflight;
-  assign bus_if.w_ready  = !w_seen      && !rd_inflight;
-  assign bus_if.ar_ready = !wr_inflight && !rd_inflight;
+  assign bus_if.aw_ready = !aw_seen && !wr_inflight && !rd_inflight;
+  assign bus_if.w_ready  = !w_seen  && !wr_inflight && !rd_inflight;
+  // Reflects write priority
+  assign bus_if.ar_ready = !rd_inflight && !(bus_if.aw_valid || aw_seen || wr_inflight);
 
   always_ff @(posedge clk_i) begin
     if (rst_i) begin
       aw_seen     <= 1'b0;
       w_seen      <= 1'b0;
+
+      ar_hs_d1    <= 1'b0;
       
       wr_inflight <= 1'b0;
       rd_inflight <= 1'b0;
@@ -60,11 +67,13 @@ module ram_bus_adapter #(
 
       if (aw_hs) begin
         aw_addr_q <= bus_if.aw_addr;
-        aw_seen <= 1'b1;
+        aw_seen   <= 1'b1;
       end
+
       if (w_hs) begin 
-        w_data_q  <= bus_if.w_data;
-        w_seen  <= 1'b1; 
+        w_strb_q <= bus_if.w_strb;
+        w_data_q <= bus_if.w_data;
+        w_seen   <= 1'b1; 
       end
 
       if ((aw_hs || aw_seen) && (w_hs || w_seen) && !wr_inflight) begin
@@ -80,33 +89,51 @@ module ram_bus_adapter #(
       end
 
       if (ar_hs) begin
+        ar_hs_d1       <= 1'b1;
         rd_inflight    <= 1'b1;
         ar_addr_q      <= bus_if.ar_addr; 
-        bus_if.r_valid <= 1'b1;
+      end else begin
+        ar_hs_d1       <= 1'b0;
       end
-
-      if (r_hs) begin
-        rd_inflight    <= 1'b0;
+      
+    // Read pipeline
+    // Since memory is synchronous, r_valid must be high 1 cycle after ar_hs
+      if (ar_hs_d1) begin
+        bus_if.r_valid <= 1'b1;
+      end else if (r_hs) begin
         bus_if.r_valid <= 1'b0;
+        rd_inflight    <= 1'b0;
       end
     end
   end
 
-  // routing
+// Routing
   always_comb begin
     mem_wr_en_o   = 1'b0;
+    mem_byte_en_o = '0;
     mem_addr_o    = '0;
     mem_wr_data_o = '0;
-    bus_if.r_data = '0;
 
-    if ((aw_seen || aw_hs) && (w_seen || w_hs)) begin
-      mem_wr_en_o   = 1'b1; 
+  // Read datapath
+    bus_if.r_data = mem_r_data_i;                        
+
+  // Write routing
+    if ((aw_seen || aw_hs) && (w_seen || w_hs) && !wr_inflight) begin
+      mem_wr_en_o   = 1'b1;
+
+    // If currently address handshaking, use bus signals.
+    // Else use latched _q.
       mem_addr_o    = aw_hs ? bus_if.aw_addr : aw_addr_q;
+
+    // If currently data handshaking, use bus signals.
+    // Else use latched _q.
+      mem_byte_en_o = w_hs  ? bus_if.w_strb  : w_strb_q;
       mem_wr_data_o = w_hs  ? bus_if.w_data  : w_data_q;
-    end
+    end 
+
+  // Read routing
     else if (rd_inflight || ar_hs) begin
       mem_addr_o    = ar_hs ? bus_if.ar_addr : ar_addr_q;
-      bus_if.r_data = mem_r_data_i;                         // TODO buffer r_data for mem's synchrous read
     end
   end
   
